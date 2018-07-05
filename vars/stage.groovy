4d7bc723d0e2eea36712b84fa3eb5a8fd424fa69
Matt Hicks <mhicks@cloudbees.com>
2018-07-05 09:37:38 -0400

    fix param handling

--- vars/stage.groovy
+++ vars/stage.groovy
@@ -1,7 +1,7 @@
-def call(String name) {
+def call(Map params = [:]) {
 
     println("Before stage call")
-    def result = steps.stage(name)
+    def result = steps.stage(params)
     println("After stage call")
     return result
 
